c743e571b7dd941e246e5a4d738a242d66f4c9bd
MarinaV27 <Vasilishinamari@gmail.com>
2024-02-17 11:27:56 +0200

    task-2 stop

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,30 @@
+class Storage {
+    #items
+    constructor(items){
+    this.#items = items;
+    }
+    getItems() {
+return this.#items;
+    }
+
+    addItem(newItem){
+        this.#items.push(newItem);
+
+    }
+
+    removeItem(itemToRemove){
+        const deletIndex = this.#items.indexOf(itemToRemove);
+        if (deletIndex != -1) {
+            this.#items.splice(deletIndex, 1)
+        }
+
+
+    }
+
+
+} 
+
+    
 
 
 
